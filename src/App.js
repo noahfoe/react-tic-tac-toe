b666639170fec2f8ace6799e5870bf0fda11794a
noahfoe <noahfoley6@gmail.com>
2021-11-16 16:29:11 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <header className="App-header">
+        <p>
+          New Tic Tac Toe Project.
+        </p>
+      </header>
     </div>
   );
 }
