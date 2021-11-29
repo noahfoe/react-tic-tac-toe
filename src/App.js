afe6afc56035d244d0d4dfe5feedd464f0ee77de
noahfoe <noahfoley6@gmail.com>
2021-11-28 22:14:01 -0600

    adds static 4x4 grid, tries to make it dynamic, but it didn't work

--- src/App.js
+++ src/App.js
@@ -4,10 +4,24 @@ import Square from "./Components/Square";
 import { WinCons } from './WinCons';
 
 function App() {
-  const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""]) // 9 boxes (3x3 grid)
+  /*
+  let playingBoard = [];
+  const chooseBoardSize = (num) => {
+    let board = [];
+    for(let i = 0; i<num; i++) {
+      board.push("");
+    }
+    console.log(board);
+    return board;
+  }
+
+  playingBoard = chooseBoardSize(9);
+  const squareRoot = Math.sqrt(playingBoard.length);
+  */
+
+  const [board, setBoard] = useState(["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",]); // 16 boxes (4x4 grid)
   const [player, setPlayer] = useState("O");
   const [result, setResult] = useState({winner: "none", state: "none"});
-
   // Called every time the board is updated
   useEffect(() => {
     checkIfTie();
@@ -18,6 +32,7 @@ function App() {
       setPlayer("X");
     }
   }, [board]);
+
 
   // Called every time the result is updated
   useEffect(() => {
@@ -67,10 +82,13 @@ function App() {
     }
   };
 
+
+
   const restartGame = () => {
-    setBoard(["", "", "", "", "", "", "", "", ""]);
+    setBoard(["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",]);
     setPlayer("O");
   };
+
 
   return (
     <div className="App">
@@ -79,20 +97,30 @@ function App() {
           <Square val={board[0]} chooseSquare={() => {chooseSquare(0);}}/>
           <Square val={board[1]} chooseSquare={() => {chooseSquare(1);}}/>
           <Square val={board[2]} chooseSquare={() => {chooseSquare(2);}}/>
+          <Square val={board[3]} chooseSquare={() => {chooseSquare(3);}}/>
         </div>
         <div className="row">
-          <Square val={board[3]} chooseSquare={() => {chooseSquare(3);}}/>
           <Square val={board[4]} chooseSquare={() => {chooseSquare(4);}}/>
           <Square val={board[5]} chooseSquare={() => {chooseSquare(5);}}/>
-        </div>
-        <div className="row">
           <Square val={board[6]} chooseSquare={() => {chooseSquare(6);}}/>
           <Square val={board[7]} chooseSquare={() => {chooseSquare(7);}}/>
+        </div>
+        <div className="row">
           <Square val={board[8]} chooseSquare={() => {chooseSquare(8);}}/>
+          <Square val={board[9]} chooseSquare={() => {chooseSquare(9);}}/>
+          <Square val={board[10]} chooseSquare={() => {chooseSquare(10);}}/>
+          <Square val={board[11]} chooseSquare={() => {chooseSquare(11);}}/>
+        </div>
+        <div className="row">
+          <Square val={board[12]} chooseSquare={() => {chooseSquare(12);}}/>
+          <Square val={board[13]} chooseSquare={() => {chooseSquare(13);}}/>
+          <Square val={board[14]} chooseSquare={() => {chooseSquare(14);}}/>
+          <Square val={board[15]} chooseSquare={() => {chooseSquare(15);}}/>
         </div>
       </div>
     </div>
   );
 }
 
+//export const playingBoard = playingBoard;
 export default App;
