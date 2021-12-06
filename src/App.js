5b9f64e7de967d0aebeb36f8a62187db18267435
noahfoe <noahfoley6@gmail.com>
2021-12-06 09:27:07 -0600

    squash merge Custom-Color

--- src/App.js
+++ src/App.js
@@ -28,6 +28,11 @@ function App() {
   const [player, setPlayer] = useState("O");
   const [result, setResult] = useState({winner: "none", state: "none"});
 
+  const [activeSquareColor, setActiveSquareColor] = useState('#64f4c4');
+  const [boardColor, setBoardColor] = useState('#7fffd4');
+  const [xColor, setXColor] = useState('#ff0000');
+  const [oColor, setOColor] = useState('#0000ff');
+
   const gameOverElement = document.getElementById('gameOver');
   const winningMessageElement = document.querySelector('[data-winning-message]');
   
@@ -127,6 +132,18 @@ function App() {
     setWinCons(newWinCons);
   };
 
+  const resetColors = () => {
+    const defaultOColor = '#0000ff';
+    const defaultXColor = '#ff0000';
+    const defaultBoardColor = '#7fffd4';
+    const defaultActiveBoardSquare = '#64f4c4'
+
+    setOColor(defaultOColor);
+    setXColor(defaultXColor);
+    setBoardColor(defaultBoardColor);
+    setActiveSquareColor(defaultActiveBoardSquare);
+  }
+
   return (
     <div className="App">
     <div className="title">Ultimate Tic-Tac-Toe</div>
@@ -137,18 +154,29 @@ function App() {
         <input type="radio" value="36" name="boardSize" onChange={val => onChangeValue(val)}/> 6x6
       </div>
       <div className="currPlayer">Current Player is: '{player}'</div>
-      <div className="board" id="board">
+      <div className="board" id="board" style={{ backgroundColor: boardColor }}>
           {rows.map((row, y) => (
             <div key={y} className="row">
               {board.slice(y * rows.length, y * rows.length + rows.length).map((square, x) => {
                 const index = y * rows.length + x;
                 return (
-                  <Square key={index} val={board[index]} chooseSquare={() => {chooseSquare(index);}}/>
+                  <Square key={index} val={board[index]} chooseSquare={() => {chooseSquare(index);}} colors={{ 'X': xColor, 'O': oColor }} activeColor={activeSquareColor} />
                 );
               })}
             </div>
           ))}
       </div>
+      <div className="colorInputs">
+        <label>Board: <input type="color" value={boardColor} onChange={e => setBoardColor(e.target.value)} /></label>
+        <label>Active Square: <input type="color" value={activeSquareColor} onChange={e => setActiveSquareColor(e.target.value)} /></label>
+        <label>X: <input type="color" value={xColor} onChange={e => setXColor(e.target.value)} /></label>
+        <label>O: <input type="color" value={oColor} onChange={e => setOColor(e.target.value)} /></label>
+        
+        <div className="resetColors">
+          <button className="resetColorsButton" onClick={resetColors}>Reset Colors</button>
+        </div>
+      </div>
+      
       <div className="winning-message" id="gameOver">
         <div data-winning-message></div>
         <button onClick={restartGame} id="restartButton">Restart</button>
--- src/Components/Square.js
+++ src/Components/Square.js
@@ -2,8 +2,8 @@ import React from 'react'
 import "../App.css";
 
 // val is X or O
-function Square({val, chooseSquare}) {
-    return (<div className="square" onClick={chooseSquare}>{val}</div>)
+function Square({val, chooseSquare, colors, activeColor}) {
+    return (<div className="square" onClick={chooseSquare} style={{ color: colors[val], '--active-color': activeColor }}>{val}</div>)
 }
 
 export default Square
