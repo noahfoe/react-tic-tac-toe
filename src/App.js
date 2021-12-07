693e79399d6f18bd38f7a3abde857da6da801d6e
noahfoe <noahfoley6@gmail.com>
2021-12-07 12:44:55 -0600

    adds option for 3 and 4 players & adds styling - Khang added the 3 and 4 players portion, but had trouble with github so wanted me to commit and push it

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {useState, useEffect} from 'react'
+import React, {useState, useEffect} from 'react'
 import Square from "./Components/Square";
 import getWinCons from './WinCons';
 import Sound from 'react-sound';
@@ -10,6 +10,7 @@ let playingBoard = [];
 let newPlayingBoard = [];
 let newWinCons = [];
 let newPlayer;
+
 function App() { 
   // Creates a blank array of size "num" for the playing board
   const chooseBoardSize = (num) => {
@@ -25,17 +26,21 @@ function App() {
   }
   
   let WinCons = getWinCons(playingBoard);
-
   const [winCons, setWinCons] = useState(WinCons);
   const [board, setBoard] = useState(playingBoard);
   const [player, setPlayer] = useState("O");
+  
+  const [numberOfPlayers, setNumberOfPlayers] = useState(2);
+
   const [result, setResult] = useState({winner: "none", state: "none"});
 
   const [activeSquareColor, setActiveSquareColor] = useState('#64f4c4');
   const [boardColor, setBoardColor] = useState('#7fffd4');
   const [xColor, setXColor] = useState('#ff0000');
   const [oColor, setOColor] = useState('#0000ff');
-
+  const [yColor, setYColor] = useState('#00ff11');
+  const [zColor, setZColor] = useState('#87217a');
+  const [currBoardSize, setCurrBoardSize] = useState(9)
   const [isPlayingClick, setIsPlayingClick] = useState(false);
   const [isPlayingGameOver, setIsPlayingGameOver] = useState(false);
 
@@ -49,18 +54,63 @@ function App() {
     checkIfTie();
     checkWinCons();
     // Ensure that X starts first if board size has been changed
-    if(newPlayer == "O") {
+    if(newPlayer === "O" && numberOfPlayers === 2) {
       setPlayer("X");
       newPlayer = "";
-    } else if(newPlayer == "X") {
+    } else if(newPlayer === "X" && numberOfPlayers === 2) {
       setPlayer("X");
       newPlayer = "";
       // Otherwise, Ensure it is next players turn
-    } else {
+    } else if(numberOfPlayers === 2) {
       if(player == "X") {
         setPlayer("O");
       } else {
         setPlayer("X");
+      }
+    }
+
+    if(newPlayer === "O" && numberOfPlayers === 3) {
+      setPlayer("X");
+      newPlayer = "";
+    } else if(newPlayer === "X" && numberOfPlayers === 3) {
+      setPlayer("X");
+      newPlayer = "";
+    } else if(newPlayer === 'Y' && numberOfPlayers === 3) {
+      setPlayer('X')
+      newPlayer = ''
+      // Otherwise, Ensure it is next players turn
+    } else if(numberOfPlayers === 3) {
+      if(player == "X") {
+        setPlayer("O");
+      } else if(player == 'O') {
+        setPlayer("Y");
+      } else {
+        setPlayer('X')
+      }
+    }
+
+    if(newPlayer === "O" && numberOfPlayers === 4) {
+      setPlayer("X");
+      newPlayer = "";
+    } else if(newPlayer === "X" && numberOfPlayers === 4) {
+      setPlayer("X");
+      newPlayer = "";
+    } else if(newPlayer === 'Y' && numberOfPlayers === 4) {
+      setPlayer('X')
+      newPlayer = ''
+    } else if(newPlayer === 'Z' && numberOfPlayers === 4) {
+      setPlayer('X')
+      newPlayer = '';
+      // Otherwise, Ensure it is next players turn
+    } else if(numberOfPlayers === 4) {
+      if(player === "X") {
+        setPlayer("O");
+      } else if(player === 'O') {
+        setPlayer("Y");
+      }else if(player === 'Y') {
+        setPlayer('Z')
+      } else {
+        setPlayer('X')
       }
     }
   }, [board]);
@@ -79,7 +129,7 @@ function App() {
 
   // Decides which square the player selected and which player selected it 
   const chooseSquare = (square) => {
-    if(board[square] != "X" && board[square] != "O") {
+    if(board[square] != "X" && board[square] != "O" && board[square] != "Y" && board[square] != "Z") {
       setBoard(board.map((val, idx) => {
         if(idx == square && val == "") {
           setIsPlayingClick(true);
@@ -133,6 +183,15 @@ function App() {
     setIsPlayingGameOver(false);
   };
 
+  const onChangePlayers = newNumberOfPlayers => {
+    setNumberOfPlayers(newNumberOfPlayers)
+      newPlayingBoard = chooseBoardSize(currBoardSize);
+      newPlayer = "O";
+      setBoard(newPlayingBoard);
+      newWinCons = getWinCons(newPlayingBoard);
+      setWinCons(newWinCons);
+  }
+
   const onChangeValue = (event) => {
     const { name, value } = event.target;
     newPlayingBoard = chooseBoardSize(parseInt(value));
@@ -140,16 +199,21 @@ function App() {
     setBoard(newPlayingBoard);
     newWinCons = getWinCons(newPlayingBoard);
     setWinCons(newWinCons);
+    setCurrBoardSize(parseInt(value))
   };
 
   const resetColors = () => {
     const defaultOColor = '#0000ff';
     const defaultXColor = '#ff0000';
+    const defaultYColor = "#00ff11";
+    const defaultZColor = "#87217a";
     const defaultBoardColor = '#7fffd4';
     const defaultActiveBoardSquare = '#64f4c4'
 
     setOColor(defaultOColor);
     setXColor(defaultXColor);
+    setYColor(defaultYColor);
+    setZColor(defaultZColor);
     setBoardColor(defaultBoardColor);
     setActiveSquareColor(defaultActiveBoardSquare);
   }
@@ -171,6 +235,14 @@ function App() {
         <input type="radio" value="25" name="boardSize" onChange={val => onChangeValue(val)}/> 5x5
         <input type="radio" value="36" name="boardSize" onChange={val => onChangeValue(val)}/> 6x6
       </div>
+      <br></br>
+      
+      <div className="radioButtonsTwo">
+        <input type="radio" value="2" defaultChecked name="numPlayer" onChange={() => onChangePlayers(2)}/> 2 Players
+        <input type="radio" value="3"  name="numPlayer" onChange={() => onChangePlayers(3)}/> 3 Players
+        <input type="radio" value="4"  name="numPlayer" onChange={() => onChangePlayers(4)}/> 4 Players
+      </div>
+      
       <div className="currPlayer">Current Player is: '{player}'</div>
       <div className="board" id="board" style={{ backgroundColor: boardColor }}>
           {rows.map((row, y) => (
@@ -178,7 +250,7 @@ function App() {
               {board.slice(y * rows.length, y * rows.length + rows.length).map((square, x) => {
                 const index = y * rows.length + x;
                 return (
-                  <Square key={index} val={board[index]} chooseSquare={() => {chooseSquare(index);}} colors={{ 'X': xColor, 'O': oColor }} activeColor={activeSquareColor} />
+                  <Square key={index} val={board[index]} chooseSquare={() => {chooseSquare(index);}} colors={{ 'X': xColor, 'O': oColor, "Y": yColor, "Z": zColor }} activeColor={activeSquareColor} />
                 );
               })}
             </div>
@@ -189,10 +261,10 @@ function App() {
         <label>Active Square: <input type="color" value={activeSquareColor} onChange={e => setActiveSquareColor(e.target.value)} /></label>
         <label>X: <input type="color" value={xColor} onChange={e => setXColor(e.target.value)} /></label>
         <label>O: <input type="color" value={oColor} onChange={e => setOColor(e.target.value)} /></label>
+        {numberOfPlayers > 2 ? <label>Y: <input type="color" value={yColor} onChange={e => setYColor(e.target.value)} /></label> : null}
+        {numberOfPlayers > 3 ? <label>Z: <input type="color" value={zColor} onChange={e => setZColor(e.target.value)} /></label> : null}     
         
-        <div className="resetColors">
-          <button className="resetColorsButton" onClick={resetColors}>Reset Colors</button>
-        </div>
+        
         <Sound
           url={click}
           playStatus={isPlayingClick ? Sound.status.PLAYING : Sound.status.STOPPED}
@@ -204,6 +276,9 @@ function App() {
           playFromPosition={0}
           onFinishedPlaying={handleOnFinishedPlayingGameOver}/>
       </div>
+      <div className="resetColors">
+          <button className="resetColorsButton" onClick={resetColors}>Reset Colors</button>
+        </div>
       <div className="sounds">
         
       </div>
